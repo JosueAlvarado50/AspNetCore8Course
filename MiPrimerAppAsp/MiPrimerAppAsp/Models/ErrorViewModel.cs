39113eff0105996d63d31512c4650add373a5f43
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2024-07-20 13:55:54 -0600

    adding new project

---
+++ MiPrimerAppAsp/MiPrimerAppAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MiPrimerAppAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
