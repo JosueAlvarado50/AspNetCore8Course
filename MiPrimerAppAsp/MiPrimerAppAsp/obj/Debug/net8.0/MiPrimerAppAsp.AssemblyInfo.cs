9a96c89a11d8482fba8f3a6e0bf704a0c41d5130
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2024-07-20 13:56:56 -0600

    adding new Folder

--- MiPrimerAppAsp/MiPrimerAppAsp/obj/Debug/net8.0/MiPrimerAppAsp.AssemblyInfo.cs
+++ MiPrimerAppAsp/MiPrimerAppAsp/obj/Debug/net8.0/MiPrimerAppAsp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiPrimerAppAsp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39113eff0105996d63d31512c4650add373a5f43")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiPrimerAppAsp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiPrimerAppAsp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
